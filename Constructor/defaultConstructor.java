83fffe6d79cbc75ffa88713c2bee09833279f974
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-06-29 18:38:29 +0600

    Create defaultConstructor.java

---
+++ Constructor/defaultConstructor.java
@@ -0,0 +1,52 @@
+class Teacher {
+    String name;
+    int id;
+    double semester;
+    double CGPA;
+    
+    Teacher(){ // Default Constructor
+        System.out.println("This is a Default Constructor"); 
+    }
+    
+    Teacher(String n, int i, double s, double cg){ // Parametrized Constructor
+        name = n;
+        id = i;
+        semester = s;
+        CGPA = cg;
+    }
+
+    void Display() {
+        // Printing out the details using println statements
+        System.out.println("Name : " + name);
+        System.out.println("ID : " + id);
+        System.out.println("Semester : " + semester);
+        System.out.println("CGPA : " + CGPA);
+        System.out.println("\n");
+
+    }
+}
+
+public class  defaultConstructor{
+    public static void main(String[] args) {
+        Teacher test1 = new Teacher("Himel Sarder", 22111121, 2.1, 3.55); // Creating an object of Teacher class
+
+        // Assigning values to the instance variables of Teacher object
+        // test1.name = "Himel Sarder";
+        // test1.id = 22111121;
+        // test1.semester = 2.1;
+        // test1.CGPA = 3.55;
+        
+        test1.Display();
+
+        Teacher test2 = new Teacher();
+
+        // test2.name = "Abonti Sana";
+        // test2.id = 22211122;
+        // test2.semester = 1.2;
+        // test2.CGPA = 3.35;
+        
+        test2.Display();
+
+
+    }
+}
