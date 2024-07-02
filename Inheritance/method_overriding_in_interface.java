d39630f760d8664c2e89f84b516d1de4fed0fd49
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-07-03 01:56:29 +0600

    Create method_overriding_in_interface.java

---
+++ Inheritance/method_overriding_in_interface.java
@@ -0,0 +1,33 @@
+class Men {
+
+  // method in the superclass
+  public void trolling() {
+    System.out.println("Ha..Ha..Ha.. Women");
+  }
+}
+
+class Women extends Men {
+
+  // overriding the troll() method
+  @Override
+  public void trolling() {
+    System.out.println("Ha..Ha..Ha.. Men");
+  }
+
+  // new method in subclass
+  public void stropTrolling() {
+    System.out.println("Stop This Kind of trolling");
+  }
+}
+
+class method_overriding_in_interface {
+  public static void main(String[] args) {
+
+    // create an object of the subclass
+    Women human = new Women();
+
+    // call the eat() method
+    human.trolling();
+    human.stropTrolling();
+  }
+}
