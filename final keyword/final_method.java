4290668087132cea8ccc3f03f002ff52d80d0ec0
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-07-04 14:02:18 +0600

    Create final_method.java

---
+++ final keyword/final_method.java
@@ -0,0 +1,18 @@
+class FinalDemo {
+    // create a final method
+    public final void display() {
+      System.out.println("This is a final method.");
+    }
+}
+
+class final_method extends FinalDemo {
+  // try to override final method
+  public final void display() {
+    System.out.println("The final method is overridden.");
+  }
+
+  public static void main(String[] args) {
+    Main obj = new Main();
+    obj.display();
+  }
+}
