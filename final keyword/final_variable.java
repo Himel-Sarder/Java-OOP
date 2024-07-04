8cf57a6a093e7cb04f792af70468a10318e0aa31
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-07-04 14:00:34 +0600

    Create final_variable.java

---
+++ final keyword/final_variable.java
@@ -0,0 +1,11 @@
+class final_variable {
+  public static void main(String[] args) {
+
+    // create a final variable
+    final int AGE = 32;
+
+    // try to change the final variable
+    AGE = 45;
+    System.out.println("Age: " + AGE);
+  }
+}
