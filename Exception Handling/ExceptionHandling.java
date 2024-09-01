d009c55b5a13fa066bb124c4ae7eda63eb6b822a
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-09-01 12:19:22 +0600

    Update ExceptionHandling.java

--- Exception Handling/ExceptionHandling.java
+++ Exception Handling/ExceptionHandling.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int divided = s.nextInt();
+        int divisor = s.nextInt();
+        try{
+            int result = divided / divisor;
+            System.out.println(result);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Can't divided by zero!");
+        }
+    }
+}
 
